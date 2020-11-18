f4971bd122a4c2f8efb5d63751cbff7634d14be4
EC2 Default User <ec2-user@ip-172-31-4-183.us-west-2.compute.internal>
2020-11-18 18:07:30 +0000

    chudham

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -4,17 +4,6 @@ resource "aws_ecs_cluster" "main" {
   name = "cb-cluster"
 }
 
-data "template_file" "cb_app" {
-  template = file("./templates/ecs/cb_app.json.tpl")
-
-  vars = {
-    app_image      = var.app_image
-    app_port       = var.app_port
-    fargate_cpu    = var.fargate_cpu
-    fargate_memory = var.fargate_memory
-    aws_region     = var.aws_region
-  }
-}
 
 resource "aws_ecs_task_definition" "app" {
   family                   = "cb-app-task"
